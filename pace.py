db94d47d895bd69f961d5367b76d5cf7ffa8709e
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2015-07-17 16:02:52 +0200

    Check

--- pace.py
+++ pace.py
@@ -3,32 +3,29 @@ import glob
 import gpxpy
 import gpxpy.gpx
 import datetime
-from pint import UnitRegistry
-ureg = UnitRegistry()
-ureg.define("timedelta = 1 = dt")
 
-best_paces = collections.OrderedDict()
-best_paces[1] = datetime.timedelta(minutes=10) * ureg.timedelta / ureg.kilometer
-best_paces[5] = datetime.timedelta(minutes=10) * ureg.timedelta / ureg.kilometer
-best_paces[10] = datetime.timedelta(minutes=10) * ureg.timedelta / ureg.kilometer
-best_paces[15] = datetime.timedelta(minutes=10) * ureg.timedelta / ureg.kilometer
-best_paces[20] = datetime.timedelta(minutes=10) * ureg.timedelta / ureg.kilometer
+records = collections.OrderedDict()
+records[1] = datetime.timedelta(minutes=10)
+records[5] = datetime.timedelta(minutes=10)
+records[10] = datetime.timedelta(minutes=10)
+records[15] = datetime.timedelta(minutes=10)
+records[20] = datetime.timedelta(minutes=10)
 
 for filename in glob.glob('*.gpx'):
-	file = open(filename, 'r')
-	gpx = gpxpy.parse(file)
+    file = open(filename, 'r')
+    gpx = gpxpy.parse(file)
 
-	for track in gpx.tracks:
-		if ('Running' in track.name):
-			length_km = (track.length_3d() / 1000.0) * ureg.kilometer
-			time = datetime.timedelta(seconds=track.get_duration())
-			pace = time * ureg.timedelta / length_km
-			
-			print(pace, length_km)
-			
-			for distance, record in best_paces.items():
-				if length_km > (distance * ureg.kilometer) and pace < record:
-					best_paces[distance] = pace
+    for track in gpx.tracks:
+        if "Running" in track.name:
+            length = track.length_3d() / 1000.0
+            duration = datetime.timedelta(seconds=track.get_duration())
+            pace = length
 
-for distance, record in best_paces.items():
-	print(distance * ureg.km, "record:", record)
+            print("%.2f km/min" % pace, length)
+
+            for distance, record in records.items():
+                if length > distance and pace < record:
+                    records[distance] = pace
+
+for distance, record in records.items():
+    print("%.2f km" % distance, "% km/min" % str(record))
